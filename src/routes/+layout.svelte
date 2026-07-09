9972cb76a8bb9df3367fb979d4aa16ba80e49704
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2026-07-09 19:00:07 +0100

    modify titles in meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,20 +12,21 @@
 <svelte:head
 	><link rel="icon" href="/favicon.svg" />
 	<title>liteguide</title>
-	<meta name="title" content="liteguide" />
+	<meta name="title" content="liteguide - BEST WAY TO LEARN SOFTWARE+HARDWARE+MORE" />
 	<meta name="description" content="LEARN EVERYTHING ABT COMPUTERS SUPER FAST + FUN" />
 	<meta name="color-scheme" content="dark light" />
 	<meta name="theme-color" content="#016630" />
 	
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="https://liteguide.vercel.app/" />
-	<meta property="og:title" content="liteguide" />
+	<meta property="og:title" content="liteguide - BEST WAY TO LEARN SOFTWARE+HARDWARE+MORE" />
 	<meta property="og:description" content="LEARN EVERYTHING ABT COMPUTERS SUPER FAST + FUN" />
 	<meta property="og:image" content="/banner.webp" />
 
 	<meta property="twitter:card" content="summary_large_image" />
 	<meta property="twitter:url" content="https://liteguide.vercel.app/" />
-	<meta property="twitter:title" content="liteguide" />
+	<meta property="twitter:title" content="liteguide - BEST WAY TO LEARN SOFTWARE+HARDWARE+MORE" />
 	<meta property="twitter:description" content="LEARN EVERYTHING ABT COMPUTERS SUPER FAST + FUN" />
+	<meta property="twitter:image" content="/banner.webp" />
 </svelte:head>
 {@render children()}
