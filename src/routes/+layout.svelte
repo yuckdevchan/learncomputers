e1b134d5978d2afeb81946232e106fae1be45d3e
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2026-07-09 19:01:47 +0100

    modify meta tag descriptions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,20 +13,20 @@
 	><link rel="icon" href="/favicon.svg" />
 	<title>liteguide</title>
 	<meta name="title" content="liteguide - BEST WAY TO LEARN SOFTWARE+HARDWARE+MORE" />
-	<meta name="description" content="LEARN EVERYTHING ABT COMPUTERS SUPER FAST + FUN" />
+	<meta name="description" content="LEARN SUPER FAST+FUN, ALL FREE, NO SIGN UP" />
 	<meta name="color-scheme" content="dark light" />
 	<meta name="theme-color" content="#016630" />
 	
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="https://liteguide.vercel.app/" />
 	<meta property="og:title" content="liteguide - BEST WAY TO LEARN SOFTWARE+HARDWARE+MORE" />
-	<meta property="og:description" content="LEARN EVERYTHING ABT COMPUTERS SUPER FAST + FUN" />
+	<meta property="og:description" content="LEARN SUPER FAST+FUN, ALL FREE, NO SIGN UP" />
 	<meta property="og:image" content="/banner.webp" />
 
 	<meta property="twitter:card" content="summary_large_image" />
 	<meta property="twitter:url" content="https://liteguide.vercel.app/" />
 	<meta property="twitter:title" content="liteguide - BEST WAY TO LEARN SOFTWARE+HARDWARE+MORE" />
-	<meta property="twitter:description" content="LEARN EVERYTHING ABT COMPUTERS SUPER FAST + FUN" />
+	<meta property="twitter:description" content="LEARN SUPER FAST+FUN, ALL FREE, NO SIGN UP" />
 	<meta property="twitter:image" content="/banner.webp" />
 </svelte:head>
 {@render children()}
